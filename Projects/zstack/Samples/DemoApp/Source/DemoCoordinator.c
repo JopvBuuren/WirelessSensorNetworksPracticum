d1e3462d6df208f115f3005c3304323c353c5a0e
Jop van Buuren <jopvanbuuren@live.nl>
2017-03-15 14:53:27 +0100

    changed coord

--- Projects/zstack/Samples/DemoApp/Source/DemoCoordinator.c
+++ Projects/zstack/Samples/DemoApp/Source/DemoCoordinator.c
@@ -580,7 +580,7 @@ void zb_ReceiveDataIndication( uint16 source, uint16 command, uint16 len, uint8 
            PIN_GREEN_LED,
            targetLightVal
       );   
-      
+      lightState = targetLightVal;
       sendLightReport();
     }
   }
@@ -614,7 +614,7 @@ static void sendLightReport(void)
   uint8 txOptions;
 
   // Set the data
-  pData[LIGHT_STATE_OFFSET] = ;
+  pData[LIGHT_STATE_OFFSET] = lightState;
   txOptions = AF_MSG_ACK_REQUEST;
   
   // Send the data (destination address is set to previously established binding 
